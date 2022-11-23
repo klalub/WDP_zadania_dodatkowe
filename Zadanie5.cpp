26fbb58dfe39137de0d8ea72a3dd0b5ff91dedd4
klalub <115089005+klalub@users.noreply.github.com>
2022-11-23 10:04:22 +0100

    Zadanie5

---
+++ Zadanie5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int samochod = 1024, pojazd = 2048;
+    int wrti = wrti;
+    int wrti(int());
+    double pensja = 9999.99, zarobki = 9999.99;//rozumiem że wartość "pensja" miała przyjąć wartość równą "zarobki"
+    int wartosc_wejsciowa; 
+    cin>>wartosc_wejsciowa;
+    return 0;
+}
